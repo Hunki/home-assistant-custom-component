2e2bac31623e174db68d4fd16247f5584ba8f0fa
baeksj <baeksj@gmail.com>
2023-12-01 20:58:26 +0900

    fix

--- custom_components/kiturami/climate.py
+++ custom_components/kiturami/climate.py
@@ -5,6 +5,7 @@ https://home-assistant.io/components/kiturami/
 """
 import hashlib
 import logging
+import asyncio
 from datetime import timedelta
 
 import homeassistant.helpers.config_validation as cv
@@ -88,7 +89,8 @@ class KrbAPI:
                    'AUTH-KEY': self.auth_key}
         try:
             response = await self.session.post(url, headers=headers, json=args, timeout=10)
-            _LOGGER.debug('JSON Response: %s', await response.text())
+            _LOGGER.warn('JSON Request: %s\nJSON Response: %s', args, await response.text())
+            #_LOGGER.warn('JSON Response: %s', await response.text())
             return response
         except Exception as ex:
             _LOGGER.error('Failed to Kiturami API status Error: %s', ex)
@@ -375,5 +377,7 @@ class Kiturami(ClimateEntity):
 
     async def async_update(self):
         """Retrieve latest state."""
+        _LOGGER.warn("Call async_update")
         await self.device.is_alive()
+        await asyncio.sleep(1)
         self.result = await self.device.device_mode_info()
